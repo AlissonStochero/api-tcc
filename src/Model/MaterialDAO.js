333a6c3a0811f29bdf74331c18589c7e0cf026df
AlissonStochero <alissonstochero@gmail.com>
2019-11-18 18:48:05 -0300

    edit sql MatByPat

--- src/Model/MaterialDAO.js
+++ src/Model/MaterialDAO.js
@@ -5,9 +5,7 @@ module.exports = {
         return pool.query(sql);        
     },
     metrialByPat(cod){
-        const sql="Select * from material inner join "+
-        "patrimonio on patrimonio.id_patrimonio = material.patrimonio_id_patrimonio "
-        +"and patrimonio.cod_patrimonio = "+cod;
+        const sql="select patrimonio.cod_patrimonio, material.id_material, material.nome as nome_material, material.valor_atual, material.depto_id_departamento,departamento.nome as nome_depto, departamento.unidade_id_unidade, unidade.nome as nome_unidade FROM material INNER JOIN patrimonio on material.patrimonio_id_patrimonio = patrimonio.id_patrimonio INNER JOIN departamento on material.depto_id_departamento = departamento.id_departamento INNER JOIN unidade on departamento.unidade_id_unidade = unidade.id_unidade WHERE patrimonio.cod_patrimonio="+cod;   
         return pool.query(sql)
     }
 }
